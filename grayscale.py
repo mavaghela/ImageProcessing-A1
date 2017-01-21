71582bbc8963bf6ec3209951749b99cd3acbf536
Mansi Vaghela <mvaghela@localhost.localdomain>
2017-01-21 18:46:47 -0500

    working q1a, rough copy

--- grayscale.py
+++
@@ -1,4 +0,0 @@
-from PIL import Image
-import numpy
-a = numpy.asarray(Image.open('image.jpg'))
-print(a)
---
+++ q1a.m
@@ -0,0 +1,39 @@
+Image = [1 2 3 4; 5 6 7 8];
+
+
+origF = [1 2 3; 4 5 6; 7 8 9]
+#function out = q1a(I, origF)
+F = rot90(origF);
+F = rot90(F);
+#Image = imread(I);
+imageSize = size(Image)
+filterSize = size(F)
+result = zeros(imageSize(1), imageSize(2))
+padded = padarray(Image, [1 1])
+
+
+function out = convolution(n, m, F, padded)
+  filterSize = size(F)
+  val = 0
+  for k = 1:filterSize(1)
+  tempm = m 
+    for l = 1:filterSize(2)
+      val += F(k, l) * padded(n, tempm)
+      tempm++
+     end
+     n++
+  end
+  out = val
+end
+
+
+for n = 1:imageSize(1)
+  for m = 1:imageSize(2)
+    val = convolution(n, m, F, padded)
+    result(n, m) = val     
+  end
+end
+#out = result
+#end
+
+
